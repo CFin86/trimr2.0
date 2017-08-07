5e7d120e324a76d7d225bcf55c1494d104917b73
Chris Finney <chrisfinney86@gmail.com>
2017-08-07 11:04:32 -0500

    maybe this works?

--- server/index.js
+++ server/index.js
@@ -26,6 +26,21 @@ app.use(prerender);
 configurePassport(app);
 
 
+// for live server
+var liveServer = require("live-server");
+var params = {
+	port: 3000, // Set the server port. Defaults to 3000.
+	host: "localhost", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+	root: "/Volumes/Internal External HD/TrimR/trim2.0 w: login/client", // Set root directory that's being served. Defaults to cwd.
+	open: false, // When false, it won't load your browser by default.
+	ignore: 'scss,my/templates', // comma-separated string for paths to ignore
+	file: "index.html", // When set, serve this file for every 404 (useful for single-page applications)
+	wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+	mount: [['/components', './node_modules']], // Mount a directory to a route.
+	logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+	middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+};
+// end live server
 
 app.use('/api', api);
 
@@ -36,5 +51,5 @@ app.get("*", function (req, res, next) {
         res.sendFile(path.join(__dirname, "../client/index.html"));
     }
 })
-
-app.listen(3000);
+liveServer.start(params); //this is for liveserver
+// app.listen(3000);
